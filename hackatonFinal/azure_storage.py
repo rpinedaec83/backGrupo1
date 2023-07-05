4970e4b98e42b4b8eeb4a20f0c3fec310e5603ef
rpineda <rpineda@x-codec.net>
2023-07-04 20:16:13 -0500

    Deploy Azure File Storage settings

--- hackatonFinal/azure_storage.py
+++ hackatonFinal/azure_storage.py
@@ -5,12 +5,12 @@ from storages.backends.azure_storage import AzureStorage
 class AzureMediaStorage(AzureStorage):
     account_name = os.getenv('AZURE_ACCOUNT_NAME')
     account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = 'media'
+    azure_container = 'mediagpr1'
     expiration_secs = None
 
 
 class AzureStaticStorage(AzureStorage):
     account_name = os.getenv('AZURE_ACCOUNT_NAME')
     account_key = os.getenv('AZURE_ACCOUNT_KEY')
-    azure_container = 'static'
+    azure_container = 'staticgpr1'
     expiration_secs = None
--- hackatonFinal/settings.py
+++ hackatonFinal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 #ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
+#CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
 
 SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
 if SECURE_SSL_REDIRECT:
