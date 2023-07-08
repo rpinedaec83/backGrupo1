20cc648f514fb5c9dace0ccda8ed0f211c6ac60d
Jostin Patrick Ninamango Origuela <jostin.idat@gmail.com>
2023-07-07 21:28:18 -0500

    se agrego urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,27 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework import routers
+from .views import *
+from django.views.decorators.csrf import csrf_exempt
+
+
+router = routers.DefaultRouter()
+
+router.register('api/producto', ProductoViewSet , "productos")
+router.register('api/pedido', PedidoViewSet , "pedidos")
+router.register('api/detalle_pedido', Detalle_pedidoViewSet , "detalle_pedidos")
+router.register('api/cliente', ClienteViewSet , "clientes")
 
 urlpatterns = [
     path('api/signup/', views.signup),
     path('api/login/', views.login),
     # path('test_token', views.test_token),
     path('api/prueba/', views.prueba),
+
+
+    path("", include(router.urls)),
+    path("api/agregar_producto_al_carrito/", csrf_exempt(agregar_producto_al_carrito), name='agregar_producto_al_carrito'),
+    path("api/eliminar_producto_al_carrito/", csrf_exempt(eliminar_producto_al_carrito), name='eliminar_producto_al_carrito'),
+    path("api/detalle_pedido_show/", csrf_exempt(detalle_pedido_show), name='detalle_pedido_show'),
+    path("api/get_pedido_con_cupon/", csrf_exempt(get_pedido_con_cupon), name='get_pedido_con_cupon'),
 ]
